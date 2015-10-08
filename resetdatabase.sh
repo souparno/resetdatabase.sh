943840fca1f270ba4eb244b855876108a2eba156
souparno majumder <souparno.majumder@gmail.com>
2015-10-08 07:21:30 +0530

    Update resetdatabase.sh

--- resetdatabase.sh
+++ resetdatabase.sh
@@ -2,6 +2,6 @@
 read -p "Enter UserName: " myusername
 read -p "Enter Password: " mypassword
 read -p "Enter Database: " mydatabase
-read -p "Enter Script Name:" myscript
+read -p "Enter Script Name: " myscript
 mysql -u $myusername -p"$mypassword" -e "DROP DATABASE IF EXISTS $mydatabase; CREATE DATABASE $mydatabase;"
 mysql -u root -p"$mypassword" -e "USE $mydatabase; source $myscript;"
