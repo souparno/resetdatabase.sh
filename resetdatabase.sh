80c8705bf6d6af397456f2c8e8b30011a4462cb8
souparno <souparno.majumder@gmail.com>
2015-10-08 07:18:01 +0530

    a simple shell script to source sql files to mysql server

---
+++ resetdatabase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p "Enter UserName: " myusername
+read -p "Enter Password: " mypassword
+read -p "Enter Database: " mydatabase
+read -p "Enter Script Name:" myscript
+mysql -u $myusername -p"$mypassword" -e "DROP DATABASE IF EXISTS $mydatabase; CREATE DATABASE $mydatabase;"
+mysql -u root -p"$mypassword" -e "USE $mydatabase; source $myscript;"
